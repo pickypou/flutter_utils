ffc98d798bf53c307c7a87eb659ce936e79d9e47
NouveauNom <ludo.spysschaert@orange.fr>
2024-08-31 15:42:20 +0200

    mise en place de example pour le package

---
+++ example/lib/animation/ExampleAnimation.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_utils/flutter_utils.dart';
+
+
+class ExampleAnimation extends StatelessWidget {
+   ExampleAnimation({super.key,});
+
+  final List<String> images = [
+    'assets/images/images.png',
+    'assets/images/flutter.png',
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 500,
+      width: 500,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text( 'Example d\'animation', style: ThemeConfig.textStyleText(context),
+          ),
+          const SizedBox(height: 35,),
+          Expanded(child: ImageSlider(
+              imagePaths: images,
+          useFadeTransition: false,
+            duration: const Duration(seconds: 3),
+            curve: Curves.easeInOut,
+          )
+
+          )
+
+        ],
+      ),
+    );
+  }
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,53 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_utils/flutter_utils.dart';
 
-import 'animations/animations_example.dart';
+import 'animation/ExampleAnimation.dart';
 
 void main() {
-  // configure les valeurs du thème pour cet exemple
-  ThemeConfig.primaryColor = Colors.blue;
+  // configure les valeur de théme pout le projet
+  ThemeConfig.primaryColor = Colors.green;
   ThemeConfig.secondaryColor = Colors.red;
-  ThemeConfig.textColor = Colors.green;
-  ThemeConfig.fontFamily = 'Lora';
-
+  ThemeConfig.textColor = Colors.blue;
+  ThemeConfig.fontFamily = 'Lora'; // Changer la police ici
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Utils Example',
-       theme: appTheme(context), // Utilisation direct de ThemeConfig
-      home:  const MyHomePage(),
+      title: 'Explication pour flutter-utils',
+      theme: appTheme(context),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
+  
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.sizeOf(context);
-
     return SingleChildScrollView(
-child:  SizedBox(
-  height: 800,
-  width: size.width,
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-      Text('Exemple d\'utilisation du packages ', style: ThemeConfig.titleStyleLarge(context),//Pour ThemeConfig
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('Comment me servir de futter_utils', style: ThemeConfig.titleStyleMedium(context),
+          ),
+           const SizedBox(height: 50,),
+           ExampleAnimation(),
+        ],
       ),
-      const SizedBox(height: 30,),
-      const AnimationExample(),
-    ],
-  ),
-),
     );
+
+  
   }
 }
