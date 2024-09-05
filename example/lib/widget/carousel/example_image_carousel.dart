38a302d184930f5c16cafa10b27bc9941240b27e
NouveauNom <ludo.spysschaert@orange.fr>
2024-09-05 11:04:13 +0200

    retouche ImageCarousel

--- example/lib/widget/carousel/example_image_carousel.dart
+++ example/lib/widget/carousel/example_image_carousel.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_utils/flutter_utils.dart';
 
+import '../../models/hike.dart';
+
 
 class ExampleImageCarousel extends StatelessWidget {
-  const ExampleImageCarousel({super.key});
+  final Hike hike;
+  const ExampleImageCarousel({super.key, required this.hike});
 
   @override
   Widget build(BuildContext context) {
 
-    return   const SizedBox(
-      child:  Center(
-        child:
-          ImageCarousel(
-              imageUrls: [
-                "assets/images/img1.png",
-                "assets/images/img2.png",
-                "assets/images/img3.png",
-              ]
-
+    return  Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(hike.title, style: ThemeConfig.titleStyleMedium(context),),
+          //Utilisation de imageCarousel avec une animation
+          AnimatedOpacityFade(
+              duration: const Duration(seconds: 2),
+              child: ImageCarousel(
+                  imageUrl: hike.imgUrls,
+                  height: 200.0,
+                  fraction: 3,
+                  autoPlay: true,
+              ),
           ),
+
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+          child: Text(hike.description, style: ThemeConfig.textStyleText(context),),
+          )
+
+        ],
       ),
     );
   }
