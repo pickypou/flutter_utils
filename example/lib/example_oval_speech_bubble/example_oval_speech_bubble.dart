a25536e22794df4adc29b8f45f28fec4ae652a1e
NouveauNom <ludo.spysschaert@orange.fr>
2024-09-19 15:56:08 +0200

    ajout oval_speech_bubble

---
+++ example/lib/example_oval_speech_bubble/example_oval_speech_bubble.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ExampleOvalSpeechBubble extends StatelessWidget {
+  const ExampleOvalSpeechBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      child: Text('data'),
+    );
+  }
+}
