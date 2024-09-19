de83ec847ead097182df25064a8b5ab73ffc2b8a
pickypou <ludo.spysschaert@orange.fr>
2024-09-19 11:14:25 +0200

    export speech_bubble_widget.dart

--- example/lib/animation/example_animation.dart
+++ example/lib/animation/example_animation.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_utils/flutter_utils.dart';
 
-
 class ExampleAnimation extends StatelessWidget {
-  ExampleAnimation({Key? key}) : super(key: key);
+  ExampleAnimation({super.key});
 
   final List<String> images = [
     'assets/images/img3.png',
     'assets/images/img2.png'
-    'assets/images/img1.png',
+        'assets/images/img1.png',
     'assets/images/img2.png',
     'assets/images/img1.png',
   ];
@@ -16,25 +15,24 @@ class ExampleAnimation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            'Example d\'animation',
-            style: ThemeConfig.textStyleText(context),
-          ),
-          const SizedBox(
-            height: 35,
-          ),
-          ImageSlider(
-            imagePaths: images,
-            useFadeTransition: true,
-            duration: const Duration(seconds: 2),
-            curve: Curves.easeInOut,
-          ),
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          'Example d\'animation',
+          style: ThemeConfig.textStyleText(context),
+        ),
+        const SizedBox(
+          height: 35,
+        ),
+        ImageSlider(
+          imagePaths: images,
+          useFadeTransition: true,
+          duration: const Duration(seconds: 2),
+          curve: Curves.easeInOut,
         ),
       ],
-    );
+    ));
   }
 }
