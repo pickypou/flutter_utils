6b5cf4c666491df289e20924819701c3ceb3ecca
NouveauNom <ludo.spysschaert@orange.fr>
2024-09-22 12:24:29 +0200

    SvgCustomPaint.dart

--- example/lib/example_svg_custom_painter/example_svg_custom_painter.dart
+++ example/lib/example_svg_custom_painter/example_svg_custom_painter.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_utils/flutter_utils.dart';
+import '../../theme_config.dart';
 
-import '../../theme.dart';
 
-class BulleDialogue extends StatelessWidget {
-  const BulleDialogue({super.key});
+class ExampleSvgCustomPainter extends StatelessWidget {
+  final double width;
+  final double height;
+  final double textXOffset;
+  final double textYOffset;
+
+  const ExampleSvgCustomPainter({
+    super.key,
+    this.width = 300,
+    this.height = 150,
+    this.textXOffset = 0,
+    this.textYOffset = -30, // Ajustez cette valeur pour déplacer le texte vers le haut
+  });
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.sizeOf(context);
     return Container(
-      width: size.width / 4,
-      height: 700,
+      width: width,
+      height: height,
       child: CustomPaint(
         painter: SvgCustomPainter(
           config: BubbleConfig(
-            borderColor: theme.colorScheme.secondary,
+            borderColor: Colors.cyanAccent,
             fillColor: Colors.transparent,
-            text: '\"je suis là pour vous aider à retrouver équilibre, relaxation & bien-être. Que vous souffriez de maux physiques ou émotionnels\"',
-            textStyle: textStyleTextAccueil(context), // Utilisez votre style de texte ici
-            width: size.width / 4,
-            height: 700,
+            text: "Je suis là pour vous aider à retrouver équilibre, relaxation & bien-être. Que vous souffriez de maux physiques ou émotionnels",
+            textStyle: const TextStyle(fontSize: 14,),
+            width: width,
+            height: height,
+            textXOffset: textXOffset,
+            textYOffset: textYOffset,
           ),
         ),
       ),
---
+++ example/lib/theme_config.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ThemeConfig {
+  // Couleurs configurables
+  static Color primaryColor = Colors.green[50]!;
+  static Color secondaryColor = const Color(0xFF000000);
+  static Color textColor = Colors.white;
+
+  // Police configurables
+  static String fontFamily = 'Lora';
+
+  // Styles de texte configurables
+  static TextStyle titleStyle(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+    double titleFontSize = size.width / 11;
+
+    return GoogleFonts.getFont(fontFamily).copyWith(
+      fontSize: titleFontSize,
+      color: secondaryColor,
+      fontWeight: FontWeight.bold,
+      decoration: TextDecoration.none,
+    );
+  }
+
+  static TextStyle titleStyleLarge(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+    double titleFontSize = size.width / 12;
+
+    return GoogleFonts.getFont(fontFamily).copyWith(
+      fontSize: titleFontSize,
+      fontWeight: FontWeight.bold,
+      color: secondaryColor,
+      decoration: TextDecoration.none,
+    );
+  }
+
+  static TextStyle titleStyleMedium(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+    double titleFontSize = size.width / 20;
+
+    return GoogleFonts.getFont(fontFamily).copyWith(
+      fontSize: titleFontSize,
+      fontWeight: FontWeight.bold,
+      color: secondaryColor,
+      decoration: TextDecoration.none,
+    );
+  }
+
+  static TextStyle titleStyleSmall(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+    double titleFontSize = size.width /50;
+
+    return GoogleFonts.getFont(fontFamily).copyWith(
+      fontSize: titleFontSize,
+      fontWeight: FontWeight.bold,
+      color: secondaryColor,
+      decoration: TextDecoration.none,
+    );
+  }
+
+  static TextStyle textStyleText(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+    double textFontSize = size.width / 90;
+
+    return GoogleFonts.getFont(fontFamily).copyWith(
+      fontSize: textFontSize,
+      color: textColor,
+      decoration: TextDecoration.none,
+    );
+  }
+}
+
+// Thème général
+ThemeData appTheme(BuildContext context) {
+  return ThemeData(
+    primaryColor: ThemeConfig.primaryColor,
+    secondaryHeaderColor: ThemeConfig.secondaryColor,
+    textTheme: TextTheme(
+      headlineLarge: ThemeConfig.titleStyle(context), // Passez directement un TextStyle
+    ),
+  );
+}
