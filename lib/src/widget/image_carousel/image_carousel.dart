b5b59a780ee0feae2ce4979a666a5a7024c4de7c
NouveauNom <ludo.spysschaert@orange.fr>
2024-09-27 07:00:07 +0200

    image_carousel.dart

--- lib/src/widget/image_carousel/image_carousel.dart
+++ lib/src/widget/image_carousel/image_carousel.dart
@@ -7,6 +7,7 @@ class ImageCarousel extends StatelessWidget {
   final double height;
   final double fraction;
   final bool autoPlay;
+  final bool isFromAssets;  // Nouveau paramètre pour différencier les images
   final Widget? animation;
 
   // Le constructeur utilise un paramètre nommé
@@ -16,37 +17,46 @@ class ImageCarousel extends StatelessWidget {
     required this.height,
     required this.fraction,
     required this.autoPlay,
-    this.animation});
+    required this.isFromAssets,  // Paramètre requis pour indiquer la source des images
+    this.animation,
+  });
 
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.sizeOf(context);
     final bool isLandscape = size.orientation() == SizeOrientation.paysage;
-    return OrientationSizeBox(size: size,
-        fraction: fraction,
-    child: CarouselSlider( options: CarouselOptions(
-      height: height,
-    viewportFraction: isLandscape ? fraction : 1.0,
-    autoPlay: autoPlay,
-    ),
-    items: imageUrl.map((url) {
-      return Builder(builder: (BuildContext context) {
-        return Container(
-          width: size.width * (isLandscape ? fraction : 1),
-          margin: const EdgeInsets.symmetric(horizontal: 5.0),
-          decoration: const BoxDecoration(
-            color: Colors.white
-          ),
-          child: animation ?? Image.network(
-            url,
-            fit: BoxFit.cover
-          ),
-        );
-      },
-      );
-    }).toList(),
-    ),);
 
-
+    return OrientationSizeBox(
+      size: size,
+      fraction: fraction,
+      child: CarouselSlider(
+        options: CarouselOptions(
+          height: height,
+          viewportFraction: isLandscape ? fraction : 1.0,
+          autoPlay: autoPlay,
+        ),
+        items: imageUrl.map((url) {
+          return Builder(builder: (BuildContext context) {
+            return Container(
+              width: size.width * (isLandscape ? fraction : 1),
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              child: animation ??
+                  (isFromAssets
+                      ? Image.asset(
+                          url,
+                          fit: BoxFit.cover,
+                        )
+                      : Image.network(
+                          url,
+                          fit: BoxFit.cover,
+                        )),
+            );
+          });
+        }).toList(),
+      ),
+    );
   }
 }
