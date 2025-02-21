58ce7030de053f83a0b5715d0479c7226e8cbbfb
NouveauNom <ludo.spysschaert@orange.fr>
2025-02-21 06:53:32 +0100

    export 'src/image_carousel/crop_config.dart';

--- lib/flutter_utils.dart
+++ lib/flutter_utils.dart
@@ -9,4 +9,5 @@ export 'src/orientation/size_extensions.dart';
 export 'src/theme/theme_config.dart';
 export 'src/widget/custom_text_field/custom_text_field.dart';
 export 'src/image_carousel/image_carousel.dart';
+export 'src/image_carousel/crop_config.dart';
 export 'src/svg_custom_painter/svg_custom_painter.dart';
